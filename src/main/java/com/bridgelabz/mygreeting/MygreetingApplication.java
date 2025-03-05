c35850b2b7e6436e6efc8a4b4aefd59ff6cc7dd5
Vivek Omkar Machade <vivekmachade323@gmail.com>
2025-03-05 13:05:35 +0530

    first commit

---
+++ src/main/java/com/bridgelabz/mygreeting/MygreetingApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.mygreeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MygreetingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MygreetingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/mygreeting/MygreetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.mygreeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MygreetingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
